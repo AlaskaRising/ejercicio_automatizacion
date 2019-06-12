8cce24686e222306c3b044d2a561dd93361cdef9
Estudiantes <estudiantes@ejemplo.com>
2019-06-12 15:26:23 -0500

    Se agregan pruebas unitarias

---
+++ test_pares.py
@@ -0,0 +1,10 @@
+import unittest
+from pares import Numeros
+
+class test_saber_si_es_par(unittest.TestCase):
+    
+    def test_numero(self):
+        self.assertEqual(0, Numeros.saber_si_es_par(self, 3))
+        self.assertEqual(1, Numeros.saber_si_es_par(self, 8))
+
+unittest.main(argv=['ignored', '-v'], exit=False)
