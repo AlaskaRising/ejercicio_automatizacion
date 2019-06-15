7253a868657d0e295c239770c6cdcd2074b18a4c
Miavega <miguel_9504@hotmail.com>
2019-06-15 14:01:13 -0500

    Se añade imagen docker con python 3 y jenkins

---
+++ prueba.py
@@ -0,0 +1,8 @@
+import os
+
+os.system('behave')
+
+os.system('python test_pares.py')
+
+if __name__ == "__main__":
+    pass
--- test_pares.py
+++ test_pares.py
@@ -2,7 +2,7 @@ import unittest
 from pares import Numeros
 
 class test_saber_si_es_par(unittest.TestCase):
-    
+
     def test_numero(self):
         self.assertEqual(0, Numeros.saber_si_es_par(self, 3))
         self.assertEqual(1, Numeros.saber_si_es_par(self, 8))
